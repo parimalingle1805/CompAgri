27f1f7ed20031aed6ca1a42f0e19120c059ccc84
DESKTOP-NQQOBK0\Parimal Ingle <parimal.ingle18@gmail.com>
2020-12-17 20:04:01 +0530

    Finished Shop page

--- client/src/components/shop/Shop.js
+++ client/src/components/shop/Shop.js
@@ -3,8 +3,10 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import {Navbar,Nav,NavDropdown,Form,FormControl,Button,Container,Row,Col,Card,ListGroup,ListGroupItem,Modal} from "react-bootstrap";
-
-
+import seed from './seed.jpg';
+import fert from './fert.jpg';
+import mach from './mach.png';
+import pest from './pest.jpg';
 class Dashboard extends Component {
   onLogoutClick = e => {
     e.preventDefault();
@@ -36,7 +38,65 @@ return (
             </Navbar.Collapse>
         </Navbar>
         
-       
+       <div>
+         <div style={{display:"flex", justifyContent:"center", marginTop:50}}>
+           <h1>Products you can buy</h1>
+         </div>
+         <div style={{display:"grid",gridTemplateColumns:"50% 50%", marginTop:100}}>
+           <div style={{display:"flex", justifyContent:"center"}}>
+             <h2>1. Seeds</h2>
+             
+             <div style={{marginLeft:100}}>
+             <img src={seed} style={{width:"100%"}}></img>
+             <h3 style={{marginTop:30}}>Price starting from: Rs. 100 </h3>
+                <Button variant="primary" style={{borderRadius:"20px", marginTop:20, marginBottom:100}}>
+                  Buy Now
+                </Button>
+             </div>
+           </div>
+
+
+           <div style={{display:"flex", justifyContent:"center"}}>
+             <h2>2. Fertilizers</h2>
+             
+             <div style={{marginLeft:100}}>
+             <img src={fert} style={{width:"100%"}}></img>
+             <h3 style={{marginTop:20}}>Price starting from: Rs. 500 </h3>
+                <Button variant="primary" style={{borderRadius:"20px", marginTop:20,marginBottom:100}}>
+                  Buy Now
+                </Button>
+             </div>
+           </div>
+
+
+           <div style={{display:"flex", justifyContent:"center"}}>
+             <h2>3. Pesticides</h2>
+             
+             <div style={{marginLeft:100}}>
+             <img src={pest} style={{width:"100%"}}></img>
+             <h3 style={{marginTop:20}}>Price starting from: Rs. 500 </h3>
+                <Button variant="primary" style={{borderRadius:"20px", marginTop:20,marginBottom:100}}>
+                  Buy Now
+                </Button>
+             </div>
+           </div>
+
+
+           <div style={{display:"flex", justifyContent:"center"}}>
+             <h2>4. Tools</h2>
+             
+             <div style={{marginLeft:100}}>
+             <img src={mach} style={{width:"100%"}}></img>
+             <h3 style={{marginTop:100}}>Price starting from: Rs. 10000 </h3>
+                <Button variant="primary" style={{borderRadius:"20px", marginTop:20,marginBottom:100}}>
+                  Buy Now
+                </Button>
+             </div>
+           </div>
+          
+
+         </div>
+       </div>
   
         
 
