9371fb0ceb5a39a25a99250c7effc9037735b429
DESKTOP-NQQOBK0\Parimal Ingle <parimal.ingle18@gmail.com>
2020-12-17 13:21:17 +0530

    made ready for heroku

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -8,6 +8,8 @@ var bodyParser = require("body-parser");
 
 var passport = require("passport");
 
+var path = require('path');
+
 var users = require("./routes/api/users");
 
 var app = express(); // Bodyparser middleware
@@ -33,7 +35,16 @@ app.use(passport.initialize()); // Passport config
 require("./config/passport")(passport); // Routes
 
 
-app.use("/api/users", users);
+app.use("/api/users", users); //serve static assets
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express["static"](client / build));
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 var port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 
 app.listen(port, function () {
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 
@@ -30,6 +31,16 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
+
+//serve static assets
+if (process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static(client/build));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
